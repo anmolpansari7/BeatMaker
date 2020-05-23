761e1a9bf010d4dc155a82836572e390c5b57f6c
anmolpansari7 <62506281+anmolpansari7@users.noreply.github.com>
2020-05-24 02:40:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -599,7 +599,7 @@ function myFunction(x) {
   }
 }
 
-var x = window.matchMedia("(max-width: 360px)");
+var x = window.matchMedia("(max-width: 767px)");
 myFunction(x); // Call listener function at run time
 x.addListener(myFunction); // Attach listener function on state changes
 
